0a0fe39797d6ad453aee573e4420596a24f518ee
suchengpo <M10507317@mail.ntust.edu.tw>
2016-12-17 17:33:58 +0800

    LOGIN完成&美化
    LOGIN完成&美化

--- TeachLink/app/src/main/java/com/example/a303/teachlink/Loging.java
+++ TeachLink/app/src/main/java/com/example/a303/teachlink/Loging.java
@@ -2,12 +2,21 @@ package com.example.a303.teachlink;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.util.ArrayMap;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Loging extends AppCompatActivity {
     private EditText ET_Username,ET_Password;
@@ -15,6 +24,7 @@ public class Loging extends AppCompatActivity {
     private User user;
     private String username,password;
     private int identity;//0:身分錯誤，1:教師，2:學生
+    private static Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +33,45 @@ public class Loging extends AppCompatActivity {
 
         findViews();
         setBTListener();
+
+        mHandler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                //What you want to do with these data? Just write here!
+                super.handleMessage(msg);
+                Bundle bundle= msg.getData();
+                int select=msg.what;
+                /*num 1: login*/
+                String content = bundle.getString("content");//json
+
+
+
+                try {
+                    JSONObject obj = new JSONObject(content);
+                    String job,accept;
+                    int jobint,acceptint;
+                    job=obj.getString("job");
+                    accept=obj.getString("login");
+                    jobint=Integer.valueOf(job);
+                    acceptint=Integer.valueOf(accept);
+                    if (acceptint==0){
+                        jobint=0;
+                    }
+                    checkIdentity(jobint);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+
+
+                //Toast.makeText(Loging.this, content, Toast.LENGTH_SHORT).show();
+
+
+            }
+        };
     }
+
 
     private void setBTListener() {
         BT_Login.setOnClickListener(new View.OnClickListener() {
@@ -31,14 +79,25 @@ public class Loging extends AppCompatActivity {
             public void onClick(View v) {
                 username=ET_Username.getText().toString();
                 password=ET_Password.getText().toString();
-                checkIdentity();
+                try {
+                    URL url = new URL("http://192.168.1.170");
+                    ArrayMap<String , String> reqData = new ArrayMap();
+                    reqData.put("select","login");
+                    reqData.put("username",username);
+                    reqData.put("password",password);
+                    WebData webData = new WebData(url,mHandler);
+                    webData.setReqData(reqData);
+                    webData.getData();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
 
-    private void checkIdentity() {
+    private void checkIdentity(int idget) {
         Context context=this;
-        identity=1;//假資料1
+        this.identity=idget;
         user=new User(username,password,identity);
         if (identity==1)//師
         {
@@ -59,7 +118,6 @@ public class Loging extends AppCompatActivity {
         else//錯誤
         {
             Toast.makeText(context,R.string.ErrorMessage1,Toast.LENGTH_LONG).show();
-
         }
     }
 
---
+++ TeachLink/app/src/main/java/com/example/a303/teachlink/WebData.java
@@ -0,0 +1,102 @@
+package com.example.a303.teachlink;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+/**
+ * Created by ray on 2016/12/14.
+ */
+
+public class WebData {
+    private URL url=null;
+    private Handler mHandler;
+    private Map<String,String> reqData;
+    private String query=null;
+    //private UserInfo user;
+    public WebData(URL url,Handler mHandler){
+        this.url=url;
+        this.mHandler=mHandler;
+    }
+
+    public void getData(){
+        new Thread(new Runnable() {
+            public void run() {
+
+                HttpURLConnection urlConnection = null;
+                try {
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setReadTimeout(5000);
+                    urlConnection.setConnectTimeout(5000);
+                    urlConnection.setDoInput(true);
+                    urlConnection.setDoInput(true);
+                    urlConnection.setRequestMethod("POST");
+                    urlConnection.setUseCaches(false);
+
+                    //Open connection for sending data
+                    OutputStream os =urlConnection.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os,"UTF-8"));
+                    writer.write(query);
+                    writer.flush();
+                    writer.close();
+                    urlConnection.connect();
+
+                    //Get response content
+                    Log.d("Server conn status!!!", String.valueOf(urlConnection.getResponseCode()));
+                    InputStream dataStream = new BufferedInputStream(urlConnection.getInputStream());
+                    InputStreamReader streamReader = new InputStreamReader(dataStream,"UTF-8");
+                    BufferedReader br = new BufferedReader(streamReader);
+                    String line;
+                    line=br.readLine();
+                    Log.d("Web Content!!!!! : ",line);
+                    Bundle bundle =new Bundle();
+                    bundle.putString("content",line);
+                    //set msg
+                    Message msg = new Message();
+                    msg.what=1;
+                    msg.setData(bundle);
+                    mHandler.sendMessage(msg);
+                    //tv_showData.setText(line);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    if (urlConnection!=null) {
+                        urlConnection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+    public void setReqData(Map<String,String> reqData){
+        this.reqData=reqData;
+        Uri.Builder builder = new Uri.Builder();
+        //Append parameters to URL
+        if(reqData.get("select")=="login") {
+            builder = builder
+                    .appendQueryParameter("select",reqData.get("select"))
+                    .appendQueryParameter("username", reqData.get("username"))
+                    .appendQueryParameter("password", reqData.get("password"));
+            query = builder.build().getEncodedQuery();
+            Log.d("POST query",query);
+        }
+    }
+
+}
